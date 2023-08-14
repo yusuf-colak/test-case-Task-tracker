25b48227429d3027b97723070e22462f91afe866
Yusuf Çolak <yusuf21706@gmail.com>
2023-08-14 20:59:03 +0300

    update bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Navbar from "./pages/NavBar";
 import "react-toastify/dist/ReactToastify.css";
 import "./global.css";
 import { ToastContainer } from "react-toastify";
-import { LocaleStoreKontrol } from "./pages/localeStoreKontrol";
+import { LocaleStoreKontrol } from "./pages/LocaleStoreKontrol";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
